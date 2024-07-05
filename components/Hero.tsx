fade9f3efcd81adbba8421e975b2a11181923537
prodigytutor <david@prodigytutors.pro>
2024-07-05 11:08:27 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m David, a Senior Developer based in Philadelphia, PA 19143.
+            Hi! I&apos;m David, a Senior Developer based in Philadelphia, PA.
           </p>
 
           <a href="#about">
