1592ed37c376a9af115062c6f76809f7115173b6
prodigytutor <david@prodigytutors.pro>
2024-07-04 23:00:59 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
 // importing env.mjs in your next.config.mjs will make sure
 // your environment variables are validated at build time
 // https://env.t3.gg/
-import './data/env.mjs';
 
 const nextConfig = {
   reactStrictMode: true,
